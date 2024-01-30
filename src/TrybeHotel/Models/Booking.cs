81b15ff2cbe30180cbd8770e768a1535cc618439
brunogsr <brunogsr https://github.com/brunogsr>
2024-01-29 22:46:22 -0300

    Adicionando Models

--- src/TrybeHotel/Models/Booking.cs
+++ src/TrybeHotel/Models/Booking.cs
@@ -2,7 +2,17 @@ namespace TrybeHotel.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
- // 1. Implemente as models da aplicação
-public class Booking {
+// 1. Implemente as models da aplicação
+public class Booking
+{
+  [Key]
+  public int BookingId { get; set; }
+  public DateTime CheckIn { get; set; }
+  public DateTime CheckOut { get; set; }
+  public int GuestQuant { get; set; }
+  [ForeignKey("UserId")]
+  public User? UserId { get; set; }
+  [ForeignKey("RoomId")]
+  public Room? RoomId { get; set; }
 
 }
