0197993fda8fa088b2763ba4b43c6dad59bce202
brunogsr <brunogsr https://github.com/brunogsr>
2024-01-30 01:44:26 -0300

    Users Repository - Add - GetUserByEmail - GetUsers

--- src/TrybeHotel/Repository/UserRepository.cs
+++ src/TrybeHotel/Repository/UserRepository.cs
@@ -17,21 +17,54 @@ namespace TrybeHotel.Repository
 
         public UserDto Login(LoginDto login)
         {
-           throw new NotImplementedException();
+            throw new NotImplementedException();
         }
         public UserDto Add(UserDtoInsert user)
         {
-            throw new NotImplementedException(); 
+            var newUser = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                UserType = "Client"
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            return new UserDto
+            {
+                UserId = newUser.UserId,
+                Name = newUser.Name,
+                Email = newUser.Email,
+                UserType = newUser.UserType
+            };
         }
 
         public UserDto GetUserByEmail(string userEmail)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.FirstOrDefault(u => u.Email == userEmail);
+            if (user == null)
+            {
+                return null;
+            }
+            return new UserDto
+            {
+                UserId = user.UserId,
+                Name = user.Name,
+                Email = user.Email,
+                UserType = user.UserType
+            };
         }
 
         public IEnumerable<UserDto> GetUsers()
         {
-           throw new NotImplementedException();
+            var users = _context.Users.Select(u => new UserDto
+            {
+                UserId = u.UserId,
+                Name = u.Name,
+                Email = u.Email,
+                UserType = u.UserType
+            });
+            return users;
         }
 
     }
