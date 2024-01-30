410a1349dbfd4dbc25a0639cff98c1bc778e5ae9
brunogsr <brunogsr https://github.com/brunogsr>
2024-01-30 01:01:36 -0300

    fixing models

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -12,6 +12,6 @@ public class Hotel
 
   [ForeignKey("City")]  // Corrigido para usar o nome da propriedade de navegação
   public int CityId { get; set; }
-  public City? City { get; set; }  // Adicionado para representar a relação de navegação
-  public ICollection<Room>? Rooms { get; set; }
+  public virtual City? City { get; set; }  // Adicionado para representar a relação de navegação
+  public virtual ICollection<Room>? Rooms { get; set; }
 }
