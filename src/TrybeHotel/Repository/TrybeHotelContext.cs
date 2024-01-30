5710118cbc951017120c796406d586eee2f8af44
brunogsr <brunogsr https://github.com/brunogsr>
2024-01-30 00:09:27 -0300

    Context

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,13 +4,25 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
         Seeder.SeedUserAdmin(this);
     }
     public TrybeHotelContext() { }
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {}
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<User> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
 }
