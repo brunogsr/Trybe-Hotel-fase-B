4e68073f49912f89822470ec809a92a9f3cb8c93
brunogsr <brunogsr https://github.com/brunogsr>
2024-01-30 01:39:51 -0300

    User Controller

--- src/TrybeHotel/Controllers/UserController.cs
+++ src/TrybeHotel/Controllers/UserController.cs
@@ -1,32 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
-using TrybeHotel.Models;
 using TrybeHotel.Repository;
 using TrybeHotel.Dto;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
 
 namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("user")]
-
-    public class UserController : Controller
+    [Authorize(Roles = "Admin")]
+    public class UserController : ControllerBase
     {
         private readonly IUserRepository _repository;
+
         public UserController(IUserRepository repository)
         {
             _repository = repository;
         }
-        
+
         [HttpGet]
-        public IActionResult GetUsers(){
-            throw new NotImplementedException();
+        public IActionResult GetUsers()
+        {
+            var allUsers = _repository.GetUsers();
+            return Ok(allUsers);
         }
 
         [HttpPost]
         public IActionResult Add([FromBody] UserDtoInsert user)
         {
-            throw new NotImplementedException();
+            var email = _repository.GetUserByEmail(user.Email);
+
+            if (email != null)
+            {
+                return Conflict("User email already exists");
+            }
+
+            var addedUser = _repository.Add(user);
+
+            return Created("", addedUser);
         }
     }
 }
